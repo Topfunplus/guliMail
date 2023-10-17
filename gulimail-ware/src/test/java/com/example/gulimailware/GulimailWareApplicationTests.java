1075cab8b71eb8fcb2c49f4f2f0b7808901d47b7
yitian <1036498144@qq.com>
2023-10-17 17:06:48 +0800

    测试信息

---
+++ gulimail-ware/src/test/java/com/example/gulimailware/GulimailWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gulimailware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
